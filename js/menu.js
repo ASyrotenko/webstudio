dc024dfd500a6d94d27e8c89e09e1ce399ee2819
Andrey <andreysyrotenko@gmail.com>
2022-08-21 12:11:00 +0300

    Menu js

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,16 @@
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', toggleModal);
+  mobileMenuRef.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
